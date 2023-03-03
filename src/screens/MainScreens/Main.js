c025c72bea4778f3d34f8f4f7266ce69d0e0b046
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-03 21:41:28 +0200

    Create Main.js

---
+++ src/screens/MainScreens/Main.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Main() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontFamily: "MulishBlack" }}>Main Screen</Text>
+    </View>
+  );
+}
