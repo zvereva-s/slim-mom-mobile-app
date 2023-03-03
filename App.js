ad155d55a5649969265f136740eb5447a2dcbecd
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-03 16:14:06 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useCallback, useState } from "react";
+import "expo-dev-menu";
+import { LogBox } from "react-native";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import { Provider } from "react-redux";
+import { ThemeProvider } from "./src/shared/providers/ThemeProvider";
+import { LanguageProvider } from "./src/shared/providers/LanguageProvider";
+
+import { store } from "./src/redux/store";
+
+import Main from "./src/screens/MainScreens/Main";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+  });
 
-      <StatusBar style="auto" />
-    </View>
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  LogBox.ignoreLogs(["EventEmitter.removeListener"]);
+
+  return (
+    <Provider store={store}>
+      <LanguageProvider>
+        <ThemeProvider>
+          <Main onLayoutRootView={onLayoutRootView} />
+        </ThemeProvider>
+      </LanguageProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
