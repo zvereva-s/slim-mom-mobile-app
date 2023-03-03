be95b650bc274598505ef055b9db4f991191b728
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-03 21:37:30 +0200

    Create variables.js

---
+++ assets/styleVariables/variables.js
@@ -0,0 +1,33 @@
+export const light = {
+  colorBackgroundForm: "rgba(255, 255, 255, 0.585)",
+  colorBoxShadowOrange: "rgba(252, 132, 45, 0.5)",
+  colorWrapperKcal: "#E0E0E0",
+
+  bgColor: "#F0F1F3",
+  colorFocus: "#FC842D",
+  colorDark: "#212121",
+  colorModalCalories: "#264061",
+  colorLine: "#E0E0E0",
+  colorPlaceholder: "#9b9faa80",
+  colorText: "#4d5056",
+
+  colorSwitcherThumbBackground: "rgb(197, 255, 135)",
+  colorSwitcherBackground: "#fff",
+};
+export const dark = {
+  colorBackgroundForm: "rgba(255, 255, 255, 0.585)",
+  colorBoxShadowOrange: "rgba(252, 132, 45, 0.5)",
+  colorWrapperKcal: "#E0E0E0",
+
+  bgColor: "#4d5056",
+  colorFocus: "#FC842D",
+  colorDark: "#fff",
+  colorModalCalories: "#264061",
+  colorLine: "#E0E0E0",
+  colorPlaceholder: "#E0E0E0",
+  colorText: "#ababab",
+  backgroundSec: "#333333",
+
+  colorSwitcherThumbBackground: "rgb(110, 110, 110)",
+  colorSwitcherBackground: "rgb(145, 145, 233)",
+};
