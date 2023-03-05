33e18b85de350007f29df2ec6ad8d36feb966b2c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-05 22:12:26 +0200

    add functions to Sign In

--- src/screens/Auth/SignInScreen/SignInForm/SignInForm.js
+++ src/screens/Auth/SignInScreen/SignInForm/SignInForm.js
@@ -1,30 +1,46 @@
 import { View } from "react-native";
 
+import useForm from "../../../../shared/hooks/useForm";
 import useTranslate from "../../../../shared/hooks/useTranslate";
 
 import CustomInput from "../../../../shared/components/CustomInput/CustomInput";
 import Button from "../../../../shared/components/Button/Button";
 
-export default function SignInForm() {
+import initialState from "./initialState";
+
+export default function SignInForm({ onSubmit, navigation }) {
   const { t } = useTranslate();
+  const { state, handleChangeTextInput, handleSubmit } = useForm({
+    initialState,
+    onSubmit,
+  });
+  const { email, password } = state;
+
   return (
     <>
       <CustomInput
         placeholder={t.login}
         keyboardType="email-address"
         pattern="/^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/"
+        onChangeText={handleChangeTextInput}
+        value={email}
       />
       <CustomInput
         placeholder={t.password}
-        keyboardType="default"
+        keyboardType="visible-password"
         secureTextEntryStart={true}
         link={true}
+        onChangeText={handleChangeTextInput}
+        value={password}
       />
       <View style={{ width: "100%" }}>
         <View style={{ marginTop: 60 }}>
-          <Button text={t.signInBtn} func={() => {}} />
+          <Button text={t.signInBtn} func={handleSubmit} />
         </View>
-        <View style={{ width: "100%", marginTop: 20 }}>
+        <View
+          style={{ width: "100%", marginTop: 20 }}
+          onPress={navigation.navigate("Sign Up")}
+        >
           <Button text={t.signUpBtn} type="disabled" />
         </View>
       </View>
--- src/screens/auth/SignInScreen/SignInScreen.js
+++ src/screens/auth/SignInScreen/SignInScreen.js
@@ -1,19 +1,44 @@
+import Toast from "react-native-toast-message";
+
+import { useDispatch } from "react-redux";
+
 import { Text, View } from "react-native";
 
 import useTranslate from "../../../shared/hooks/useTranslate";
 import useTheme from "../../../shared/hooks/useTheme";
+import useAuthState from "../../../shared/hooks/useAuthState";
+
+import { signinRequest } from "../../../redux/auth/auth-operations";
+import { getErrorMessage, notify } from "../../../shared/utils/utils";
+import { toastConfig } from "../../../shared/components/Toast/toastConfig";
 
 import SignInForm from "./SignInForm/SignInForm";
 
 import BackgroundView from "../../../shared/components/BackgroundView/BackgroundView";
 import LangSwitcher from "../../../shared/components/LangSwitcher/LangSwitcher";
 import SwitcherTheme from "../../../shared/components/SwitcherTheme/SwitcherTheme";
+import Loader from "../../../shared/components/Loader/Loader";
 
 import * as themeVariables from "../../../../assets/styleVariables/variables";
 
-export default function SignInScreen() {
-  const { t } = useTranslate();
+export default function SignInScreen({ navigation }) {
+  const { t, lang } = useTranslate();
   const { theme } = useTheme();
+
+  const { error, loading } = useAuthState();
+
+  let errMessage = error && getErrorMessage(error);
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    const obj = {
+      email: data["email-address"].toLowerCase(),
+      password: data["visible-password"].toLowerCase(),
+    };
+
+    dispatch(signinRequest(obj));
+  };
 
   return (
     <BackgroundView backGdColor={themeVariables[theme].bgColor}>
@@ -28,7 +53,7 @@ export default function SignInScreen() {
         <LangSwitcher />
         <SwitcherTheme />
       </View>
-      <View style={{ alignItems: "center", paddingVertical: 100 }}>
+      <View style={{ alignItems: "center", paddingVertical: 50 }}>
         <Text
           style={{
             fontSize: 30,
@@ -38,7 +63,10 @@ export default function SignInScreen() {
         >
           {t.signInTitle}
         </Text>
-        <SignInForm />
+        {loading && <Loader bool="true" size="large" />}
+        {!loading && <SignInForm onSubmit={onSubmit} navigation={navigation} />}
+        {error && notify(errMessage[lang], "error", lang)}
+        <Toast config={toastConfig} />
       </View>
     </BackgroundView>
   );
