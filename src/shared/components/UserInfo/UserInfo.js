650b5d97b2304d8fba5d0af304e5e15a4d39509c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-11 15:04:59 +0200

    Create UserInfo.js

---
+++ src/shared/components/UserInfo/UserInfo.js
@@ -0,0 +1,92 @@
+import { useDispatch } from "react-redux";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+import useTheme from "../../hooks/useTheme";
+import useAuthState from "../../hooks/useAuthState";
+import useTranslate from "../../hooks/useTranslate";
+
+import { logoutRequest } from "../../../redux/auth/auth-operations";
+
+import LangSwitcher from "../LangSwitcher/LangSwitcher";
+import SwitcherTheme from "../SwitcherTheme/SwitcherTheme";
+
+import * as themeVariables from "../../../../assets/styleVariables/variables";
+
+export default function UserInfo() {
+  const { t } = useTranslate();
+  const { theme } = useTheme();
+
+  const dispatch = useDispatch();
+  const { user, token } = useAuthState();
+
+  const logout = () => {
+    dispatch(logoutRequest(token));
+  };
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "auto",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+        backgroundColor: themeVariables[theme].bgColor,
+        borderBottomWidth: 2,
+        borderColor: themeVariables[theme].colorLine,
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          width: "40%",
+          justifyContent: "space-between",
+        }}
+      >
+        <LangSwitcher />
+        <SwitcherTheme />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          width: "30%",
+          justifyContent: "space-around",
+          alignItems: "baseline",
+        }}
+      >
+        <Text
+          style={{
+            ...styles.userText,
+            color: themeVariables[theme].colorText,
+            fontFamily: "MulishBlack",
+          }}
+        >
+          {user?.name?.split(" ")[0] || "User"}
+        </Text>
+        <Text
+          style={{ ...styles.userText, color: themeVariables[theme].colorText }}
+        >
+          |
+        </Text>
+        <TouchableOpacity activeOpacity={0.8}>
+          <Text
+            style={{
+              ...styles.userText,
+              color: themeVariables[theme].colorText,
+            }}
+          >
+            {t.logOut}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  userText: {
+    fontFamily: "MulishRegular",
+  },
+});
