38e22c8f92413a9e7f364b9a8065135ea936fa3a
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-11 15:06:13 +0200

    create styles to backgroundViewDiary

---
+++ src/screens/MainScreens/DiaryScreen/BackgroundViewDiary/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  backGround: {
+    position: "absolute",
+    top: -120,
+    left: -100,
+  },
+});
