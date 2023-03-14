7154bf0dcabe1c9dfee5cc08285bc859d68dda18
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-14 16:31:24 +0200

    Update useForm.js

--- src/shared/hooks/useForm.js
+++ src/shared/hooks/useForm.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+import { validateField } from "../utils/utils";
 
 export default function useForm({ initialState, onSubmit }) {
   const [state, setState] = useState({ ...initialState });
+  const [error, setError] = useState({
+    message: "",
+    type: "",
+    name: "",
+  });
+  const validate = (type, value, name) => {
+    setError(validateField(type, value, name));
+  };
 
-  const handleChangeTextInput = (value, keyboardType) => {
+  const handleChangeTextInput = (value, name) => {
+    if (name === "height" || name === "weight" || name === "age") {
+      validate("number", value, name);
+      if (error?.message) {
+        return;
+      }
+    }
     setState((prevState) => ({
       ...prevState,
-      [keyboardType]: value.toLowerCase(),
+      [name]: value.toLowerCase(),
+    }));
+  };
+
+  const handleRadioInput = ({ name, value }) => {
+    setState((prevState) => ({
+      ...prevState,
+      [name]: String(value).toLowerCase(),
     }));
   };
 
@@ -21,5 +44,12 @@ export default function useForm({ initialState, onSubmit }) {
     });
   };
 
-  return { state, handleChangeTextInput, handleSubmit };
+  return {
+    state,
+    error,
+    validate,
+    handleChangeTextInput,
+    handleSubmit,
+    handleRadioInput,
+  };
 }
