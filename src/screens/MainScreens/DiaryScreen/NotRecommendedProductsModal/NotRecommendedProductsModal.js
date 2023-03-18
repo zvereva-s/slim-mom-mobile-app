99614bbf76ddedc507ddae78a3d9eff5c44eb9cd
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-18 16:30:26 +0200

    Create NotRecommendedProductsModal.js

---
+++ src/screens/MainScreens/DiaryScreen/NotRecommendedProductsModal/NotRecommendedProductsModal.js
@@ -0,0 +1,77 @@
+import { Alert, Modal, StyleSheet, Text, Pressable, View } from "react-native";
+
+import useTheme from "../../../../shared/hooks/useTheme";
+
+import NotRecommendedProducts from "../../../../shared/components/NotRecommendedProducts/NotRecommendedProducts";
+
+export default function NotRecommendedProductsModal({
+  modalVisible,
+  setModalVisible,
+}) {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <NotRecommendedProducts />
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
