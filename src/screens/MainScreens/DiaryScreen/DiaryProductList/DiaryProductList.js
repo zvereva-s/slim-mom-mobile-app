28410766e904e41cc2918cb1a4d6f9c3a0a9ad66
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-23 18:11:05 +0200

    add props

--- src/screens/MainScreens/DiaryScreen/DiaryProductList/DiaryProductList.js
+++ src/screens/MainScreens/DiaryScreen/DiaryProductList/DiaryProductList.js
@@ -1,3 +1,4 @@
+import { useDispatch } from "react-redux";
 import {
   FlatList,
   View,
@@ -9,176 +10,114 @@ import {
 import useTranslate from "../../../../shared/hooks/useTranslate";
 import useTheme from "../../../../shared/hooks/useTheme";
 
+import { deleteProductRequest } from "../../../../redux/diary/diary-operations";
+
 import Icon from "../../../../shared/components/Icon/Icon";
 
 import * as themeVariables from "../../../../../assets/styleVariables/variables";
 
-const data = [
-  {
-    id: 1,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "340",
-  },
-  {
-    id: 2,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 3,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 4,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 5,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 11,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 21,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 31,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 41,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 51,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 112,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 212,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 312,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 412,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-  {
-    id: 512,
-    product: "Tasty product",
-    weight: "350",
-    kcal: "34",
-  },
-];
-
-export default function DiaryProductList() {
+export default function DiaryProductList({ list }) {
   const { theme } = useTheme();
   const { t } = useTranslate();
+  const dispatch = useDispatch();
 
   return (
-    <FlatList
+    <View
       style={{
-        flex: 1,
-        height: 265,
+        height: "80%",
         width: "100%",
         marginTop: 20,
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      data={data}
-      keyExtractor={({ id }) => id}
-      renderItem={({ item }) => (
-        <View
-          style={{
-            width: "100%",
-            flexDirection: "row",
-            paddingHorizontal: 10,
+    >
+      {list.length > 0 && (
+        <FlatList
+          data={list}
+          keyExtractor={({ _id }) => _id}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                width: "100%",
+                flexDirection: "row",
+                paddingHorizontal: 10,
 
-            alignItems: "center",
-            justifyContent: "space-around",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <View
+                style={{
+                  ...styles.line,
+                  width: "60%",
+                  borderBottomColor: themeVariables[theme].colorLine,
+                }}
+              >
+                <Text
+                  style={{
+                    ...styles.text,
+                    color: themeVariables[theme].colorText,
+                  }}
+                >
+                  {item.name}
+                </Text>
+              </View>
+              <View
+                style={{
+                  ...styles.line,
+                  borderBottomColor: themeVariables[theme].colorLine,
+                }}
+              >
+                <Text
+                  style={{
+                    ...styles.text,
+                    color: themeVariables[theme].colorText,
+                  }}
+                >
+                  {item.weight} {t.g}
+                </Text>
+              </View>
+              <View
+                style={{
+                  ...styles.line,
+                  borderBottomColor: themeVariables[theme].colorLine,
+                }}
+              >
+                <Text
+                  style={{
+                    ...styles.text,
+                    color: themeVariables[theme].colorText,
+                  }}
+                >
+                  {item.kcal} {t.kcal}
+                </Text>
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                onPress={() => dispatch(deleteProductRequest(item._id))}
+              >
+                <Icon
+                  type="close"
+                  size="13"
+                  theme={theme}
+                  colorProp={themeVariables[theme].colorText}
+                />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      )}
+      {list.length === 0 && (
+        <Text
+          style={{
+            ...styles.text,
+            color: themeVariables[theme].colorText,
           }}
         >
-          <View
-            style={{
-              ...styles.line,
-              width: "60%",
-              borderBottomColor: themeVariables[theme].colorLine,
-            }}
-          >
-            <Text
-              style={{ ...styles.text, color: themeVariables[theme].colorText }}
-            >
-              {item.product}
-            </Text>
-          </View>
-          <View
-            style={{
-              ...styles.line,
-              borderBottomColor: themeVariables[theme].colorLine,
-            }}
-          >
-            <Text
-              style={{ ...styles.text, color: themeVariables[theme].colorText }}
-            >
-              {item.weight} {t.g}
-            </Text>
-          </View>
-          <View
-            style={{
-              ...styles.line,
-              borderBottomColor: themeVariables[theme].colorLine,
-            }}
-          >
-            <Text
-              style={{ ...styles.text, color: themeVariables[theme].colorText }}
-            >
-              {item.kcal} {t.kcal}
-            </Text>
-          </View>
-          <TouchableOpacity activeOpacity={0.8}>
-            <Icon
-              type="close"
-              size="13"
-              theme={theme}
-              colorProp={themeVariables[theme].colorText}
-            />
-          </TouchableOpacity>
-        </View>
+          {t.emptyDiaryList}
+        </Text>
       )}
-    />
+    </View>
   );
 }
 
