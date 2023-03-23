a43c2828d304d25c32f33f57b2b9ae492030eb56
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-23 18:12:17 +0200

    delete unused functions

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,23 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-
 import authReducer from "./auth/auth-slice";
 import healthyDataReducer from "./healthyData/healthyData-slice";
 import diaryReducer from "./diary/diary-slice";
 
-const persistConfig = {
-  key: "token",
-  storage: AsyncStorage,
-  whitelist: ["token"],
-};
-
-const persistAuthReducer = persistReducer(persistConfig, authReducer);
-
 const rootReducer = combineReducers({
-  auth: persistAuthReducer,
+  auth: authReducer,
   healthyDataState: healthyDataReducer,
   diary: diaryReducer,
 });
