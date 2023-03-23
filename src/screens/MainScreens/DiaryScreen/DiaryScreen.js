14e47e8438ba8058bcb025e9888ea4c6548fcee9
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-23 18:10:59 +0200

    add functions

--- src/screens/MainScreens/DiaryScreen/DiaryScreen.js
+++ src/screens/MainScreens/DiaryScreen/DiaryScreen.js
@@ -1,10 +1,21 @@
-import { View, TouchableOpacity } from "react-native";
+import { useState, useEffect } from "react";
 
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { View, TouchableOpacity } from "react-native";
 
 import useTranslate from "../../../shared/hooks/useTranslate";
 import useTheme from "../../../shared/hooks/useTheme";
 
+import { getProductListRequest } from "../../../redux/diary/diary-operations";
+import { summaryOfDayRequest } from "../../../redux/healthyData/healthyData-operations";
+
+import { productListData } from "../../../redux/diary/diary-selector";
+import {
+  summaryObj,
+  healthyDataObj,
+} from "../../../redux/healthyData/healthyData-selectors";
+
+import { getFoodList } from "../../../shared/api/diary";
 import { converToDate } from "../../../shared/utils/utils";
 
 import DiaryProductList from "./DiaryProductList/DiaryProductList";
@@ -23,14 +34,47 @@ import { styles } from "./styles";
 import * as themeVariables from "../../../../assets/styleVariables/variables";
 
 export default function DiaryScreen({ navigation }) {
-  const { t } = useTranslate();
+  const { t, lang } = useTranslate();
   const { theme } = useTheme();
+
+  const dispatch = useDispatch();
 
   const [modalVisible, setModalVisible] = useState(false);
   const [calendarVisible, setCalendarVisible] = useState(false);
 
+  const [foodListForChoose, setFoodListForChoose] = useState([]);
+
   const { year, month, day } = converToDate(new Date());
   const [date, setDate] = useState(`${day}.${month}.${year}`);
+
+  const { healthyData } = useSelector(healthyDataObj);
+  const { left, consumed, dailyRate, procentOfDayNorm } =
+    useSelector(summaryObj);
+
+  const productList = useSelector(productListData);
+  const products = productList
+    ?.map((product) => ({
+      ...product,
+      name: foodListForChoose?.find((item) => String(item.key) === product.key)
+        ?.name[lang],
+    }))
+    .filter((product) => product.date === date);
+
+  useEffect(() => {
+    async function func() {
+      try {
+        const { fullDataFood } = await getFoodList();
+        setFoodListForChoose(
+          fullDataFood.map((el) => ({ name: el.Name, key: el.key }))
+        );
+      } catch (e) {
+        throw e;
+      }
+    }
+    func();
+    dispatch(getProductListRequest());
+    dispatch(summaryOfDayRequest(date));
+  }, [products.length]);
 
   return (
     <>
@@ -56,16 +100,10 @@ export default function DiaryScreen({ navigation }) {
           >
             <Icon type="calendar" size={20} theme={theme} />
           </TouchableOpacity>
-        </View>
-        <CalendarModal
-          calendarVisible={calendarVisible}
-          setCalendarVisible={setCalendarVisible}
-          setDate={setDate}
-        />
-        <DiaryProductList />
-        <View style={{ alignItems: "center" }}>
           <TouchableOpacity
-            onPress={() => navigation.navigate("Add Product")}
+            onPress={() =>
+              navigation.navigate("Add Product", { dateProp: date })
+            }
             activeOpacity={0.8}
             style={{
               alignItems: "center",
@@ -74,12 +112,17 @@ export default function DiaryScreen({ navigation }) {
               height: 50,
               backgroundColor: themeVariables[theme].colorFocus,
               borderRadius: 50,
-              marginTop: 15,
             }}
           >
             <Icon type="plus" size={20} />
           </TouchableOpacity>
         </View>
+        <CalendarModal
+          calendarVisible={calendarVisible}
+          setCalendarVisible={setCalendarVisible}
+          setDate={setDate}
+        />
+        <DiaryProductList list={products} />
       </View>
       <BackgroundViewDiary backGdColor={themeVariables[theme].backgroundSec}>
         <Title
@@ -87,11 +130,17 @@ export default function DiaryScreen({ navigation }) {
           size={20}
           color={themeVariables[theme].colorDark}
         />
-        <SummaryList />
+        <SummaryList
+          left={left}
+          consumed={consumed}
+          dailyRate={dailyRate}
+          procentOfDayNorm={procentOfDayNorm}
+        />
         <Link text={t.titleNotRecom} func={() => setModalVisible(true)} />
         <NotRecommendedProductsModal
           modalVisible={modalVisible}
           setModalVisible={setModalVisible}
+          notAllowedProducts={healthyData.notAllowedProducts[lang]}
         />
       </BackgroundViewDiary>
     </>
