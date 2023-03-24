d0a94aa79eb1d007fa356f670af5c4dafbed1a3c
zvereva <96833638+zvereva-s@users.noreply.github.com>
2023-03-24 14:04:31 +0200

    Update CalculatorScreen.js

--- src/screens/MainScreens/CalculatorScreen/CalculatorScreen.js
+++ src/screens/MainScreens/CalculatorScreen/CalculatorScreen.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import { View, Image, ScrollView } from "react-native";
 
+import { healthyDataObj } from "../../../redux/healthyData/healthyData-selectors";
+import { dailyRateUserRequest } from "../../../redux/healthyData/healthyData-operations";
+
 import CalculatorForm from "../../MainScreens/CalculatorScreen/CalculatorForm/CalculatorForm";
+import CalculatorModal from "./CalculatorModal/CalculatorModal";
 
 import Title from "../../../shared/components/Title/Title";
 import LogoHeader from "../../../shared/components/LogoHeader/LogoHeader";
@@ -9,15 +16,25 @@ import Container from "../../../shared/components/Container/Container";
 
 import useTranslate from "../../../shared/hooks/useTranslate";
 import useTheme from "../../../shared/hooks/useTheme";
+import useAuthState from "../../../shared/hooks/useAuthState";
 
 import * as themeVariables from "../../../../assets/styleVariables/variables";
 
-export default function CalculatorScreen() {
+export default function CalculatorScreen({ navigation }) {
   const { t } = useTranslate();
   const { theme } = useTheme();
 
+  const { user } = useAuthState();
+  const { healthyData } = useSelector(healthyDataObj);
+  const { dailyRate, notAllowedProducts } = healthyData;
+
+  const dispatch = useDispatch();
+
+  const [openModal, setOpenModal] = useState(false);
+
   const onSubmit = (data) => {
-    console.log("data", data);
+    dispatch(dailyRateUserRequest({ userID: user.id, obj: data }));
+    setOpenModal(!openModal);
   };
 
   return (
@@ -47,6 +64,15 @@ export default function CalculatorScreen() {
               size={30}
             />
             <CalculatorForm onSubmit={onSubmit} />
+            {openModal && (
+              <CalculatorModal
+                navigation={navigation}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+                dailyRate={dailyRate}
+                notAllowedProducts={notAllowedProducts}
+              />
+            )}
           </Container>
         </ScrollView>
       </View>
